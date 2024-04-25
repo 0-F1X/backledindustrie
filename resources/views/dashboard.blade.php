568ad89df470a93ff0a00f9a96c00d476cf99ec4
Fiderana <fideranaandria5@gmail.com>
2024-04-25 16:34:33 +0300

    4th Commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="container">
+    <h1 class="mt-5 mb-3">Dashboard</h1>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Total Users</h5>
+            <p class="card-text">100</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Total Orders</h5>
+            <p class="card-text">50</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Total Revenue</h5>
+            <p class="card-text">$5000</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Recent Orders</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Order 1</li>
+              <li class="list-group-item">Order 2</li>
+              <li class="list-group-item">Order 3</li>
+              <li class="list-group-item">Order 4</li>
+              <li class="list-group-item">Order 5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Top Products</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Product 1</li>
+              <li class="list-group-item">Product 2</li>
+              <li class="list-group-item">Product 3</li>
+              <li class="list-group-item">Product 4</li>
+              <li class="list-group-item">Product 5</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Bootstrap Bundle with Popper -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,82 +2,29 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+        <!-- Main content goes here -->
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
+        <!-- Sidebar -->
+        @section('sidebar')
+            <div class="sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#"><i class="fas fa-tachometer-alt"></i> Tableau de Bord</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-envelope"></i> Message</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-info-circle"></i> Informations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-box"></i> Produits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-images"></i> Galerie</a>
+                    </li>
+                </ul>
             </div>
-        </div>
-
-        <h1 class="mt-5 mb-3">Dashboard</h1>
-
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Users</h5>
-                        <p class="card-text">100</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Orders</h5>
-                        <p class="card-text">50</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Revenue</h5>
-                        <p class="card-text">$5000</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row mt-5">
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Recent Orders</h5>
-                        <ul class="list-group">
-                            <li class="list-group-item">Order 1</li>
-                            <li class="list-group-item">Order 2</li>
-                            <li class="list-group-item">Order 3</li>
-                            <li class="list-group-item">Order 4</li>
-                            <li class="list-group-item">Order 5</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Top Products</h5>
-                        <ul class="list-group">
-                            <li class="list-group-item">Product 1</li>
-                            <li class="list-group-item">Product 2</li>
-                            <li class="list-group-item">Product 3</li>
-                            <li class="list-group-item">Product 4</li>
-                            <li class="list-group-item">Product 5</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @show
     </div>
 @endsection
